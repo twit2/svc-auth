bcee7288250915057d3c94a710a3b256b7cf0b32
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-10-23 12:20:24 -0700

    commit auth service boilerplate

---
+++ README.md
@@ -0,0 +1,4 @@
+# Twit2 Authentication Service
+
+This microservice is responsible for credential management and user authentication.
+
---
+++ src/Index.ts
@@ -0,0 +1,48 @@
+import { configDotenv } from 'dotenv';
+import express, { Request, Response } from 'express';
+import mongoose, { Mongoose } from 'mongoose';
+import cache from 'ts-cache-mongoose';
+
+// Load ENV parameters
+configDotenv();
+
+// Setup
+// ------------------------------------------------
+const app = express();
+const port = process.env.HTTP_PORT || 3000;
+
+app.use(express.json());
+
+// Routes
+// ------------------------------------------------
+app.get('/', (req: Request, res: Response) => {
+    res.send({ test: true });
+});
+
+/**
+ * Main entry point for program.
+ */
+async function main() {
+    if(process.env.DB_URL == null) {
+        console.error("No database URL defined - is your .env file correct?");
+        return;
+    }
+
+    // Connect to database
+    try {
+        console.log(`Connecting to ${process.env.DB_URL}...`);
+        await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
+        console.log(`Connected to database.`);
+    } catch(e) {
+        console.error("Cannot connect to database server.");
+        return;
+    }
+
+    // Listen at the port
+    app.listen(port, () => {
+        console.log(`Service active at port ${port}`);
+    });
+}
+
+main()
+    .catch(()=>process.exit(1));
